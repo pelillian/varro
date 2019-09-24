13f603f65214ad1a974f07485cb992fde8747016
Peter Lillian <plillian@usc.edu>
2019-09-23 17:38:37 -0700

    create fpga flash and interface files

---
+++ fpga-evolution/fpga/flash.py
@@ -0,0 +1,9 @@
+"""
+This module handles flashing of a bitstream file to the FPGA.
+"""
+
+
+def flash_ecp5(file):
+    """Flashes a bitstream file to the ECP5 fpga. For best performance, use a file in ramdisk."""
+    pass
+
---
+++ fpga-evolution/fpga/interface.py
@@ -0,0 +1,8 @@
+"""
+This module handles communication of data to the FPGA once it has already been flashed.
+"""
+
+
+def put_array(data):
+    """Loads input data (in numpy array or other iterable form) onto the FPGA."""
+    pass
