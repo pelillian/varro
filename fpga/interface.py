eb923f9b578ebacf648939b8168ae1934c5f0d5b
pelillian <plillian@usc.edu>
2019-10-09 00:39:12 -0700

    basic interface

--- fpga/interface.py
+++ fpga/interface.py
@@ -1,8 +1,16 @@
 """
 This module handles communication of data to the FPGA once it has already been flashed.
 """
-
+import os
 
 def put_array(data):
     """Loads input data (in numpy array or other iterable form) onto the FPGA."""
     pass
+
+"""
+Given a bitstream stored in a file (*.bit), flashes the FPGA with the file contents
+"""
+def flash_from_file(filename): 
+    # Call the Makefile containing the compile/flash statements
+    os.sys("make prog")
+
