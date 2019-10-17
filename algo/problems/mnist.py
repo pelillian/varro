dd0b0001afa4e58211b9da878c9f289e7c1b63a3
fuccichris <cfucci@usc.edu>
2019-10-17 14:34:16 -0700

    returning training data for mnist

--- algo/problems/mnist.py
+++ algo/problems/mnist.py
@@ -1,3 +1,21 @@
 """
 This module contains a function that returns the training set for mnist
 """
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import numpy as np
+import keras 
+from keras.datasets import mnist
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+import keras.backend as K
+import tensorflow as tf
+
+
+def mnistTraining(): 
+    
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    return ((x_train, y_train), (x_test, y_test))
+
+
