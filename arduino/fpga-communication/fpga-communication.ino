3c143172647db9c9323663392f8f6b444d0b6d72
AlexColello <alexlcolello@gmail.com>
2019-10-17 15:35:26 -0700

    Made changes to arduino ino file

--- arduino/fpga-communication/fpga-communication.ino
+++ arduino/fpga-communication/fpga-communication.ino
@@ -4,37 +4,38 @@ void setup()
 {
 
   Serial.begin(9600);  // initialize serial communications at 9600 bps
+  pinMode(LED_BUILTIN, OUTPUT);
+
 
 }
 
 void loop()
 {
-
-  String readString = "";
-  
+  char c = ' ';
   while(!Serial.available()) {}
-  
-   while (Serial.available())
+
+  if (Serial.available() > 0)
   {
-    if (Serial.available() >0)
-    {
-      char c = Serial.read();  //gets one byte from serial buffer
-      readString += c; //makes the string readString
-    }
+    c = Serial.read();  //gets one byte from serial buffer
   }
 
-  if (readString.length() >0)
+  if(c == 0)
   {
-    Serial.print("Arduino received: ");  
-    Serial.println(readString); //see what was received
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    Serial.println("LED set to high");
+
+  } else if(c == 1){
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+    Serial.println("LED set to low");
+    
+  } else {
+    Serial.print("Could not process input: ");    
+    Serial.println((int)c);
+
   }
-  delay(500);
 
-  // serial write section
-
-  char ard_sends = '1';
-  Serial.print("Arduino sends: ");
-  Serial.println(ard_sends);
-  Serial.print("\n");
   Serial.flush();
+
+  delay(100);
+
 }
