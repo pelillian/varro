a6a2d4d5a3428209212ac69dd7bb299cf4c56242
pelillian <plillian@usc.edu>
2019-10-20 12:48:55 -0700

    more tabs to spaces

--- varro/fpga/ecp5-test/interface.v
+++ varro/fpga/ecp5-test/interface.v
@@ -1,12 +1,12 @@
 module top(input clk, input btn, output[7:0] led);
-	reg [9:0] clock_slow; 
+    reg [9:0] clock_slow; 
 
-	always@(posedge clk) begin
-		clock_slow <= clock_slow + 1; 
-		if (clock_slow[9] == 1'b1) 
-			RASP_IO2 = 1; 
-		else
-			RASP_IO2 = 0; 
+    always@(posedge clk) begin
+        clock_slow <= clock_slow + 1; 
+        if (clock_slow[9] == 1'b1) 
+            RASP_IO2 = 1; 
+        else
+            RASP_IO2 = 0; 
 
-	end
+    end
 endmodule
