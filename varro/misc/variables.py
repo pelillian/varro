33244b50be1742490e015a186bde705c2df46300
Chengyi (Jeff) Chen <chen364@usc.edu>
2019-10-20 17:12:13 -0700

    debug: reorganized argparse arguments

--- varro/misc/variables.py
+++ varro/misc/variables.py
@@ -13,7 +13,7 @@ ABSOLUTE_ALGO_LOGS_PATH = os.path.join(ROOT_DIR, 'logs/varro/algo')
 
 # This is the folder that keeps snapshots 
 # of population in each generation of experiment
-EXPERIMENT_CHECKPOINTS_PATH = os.path.join(ROOT_DIR, 'varro/algo/experiment_snapshots')
+EXPERIMENT_CHECKPOINTS_PATH = os.path.join(ROOT_DIR, 'varro/algo/experiment_checkpoints')
 
 # Whole population will be written in a pickled 
 # dictionary every FREQ generations
