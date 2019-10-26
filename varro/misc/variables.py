aa9b691c1dcc7614675716f855231469e55121b4
pelillian <plillian@usc.edu>
2019-10-25 22:42:48 -0700

    change checkpoints dir

--- varro/misc/variables.py
+++ varro/misc/variables.py
@@ -13,7 +13,7 @@ ABSOLUTE_ALGO_LOGS_PATH = os.path.join(ROOT_DIR, 'logs/varro/algo')
 
 # This is the folder that keeps snapshots
 # of population in each generation of experiment
-EXPERIMENT_CHECKPOINTS_PATH = os.path.join(ROOT_DIR, 'varro/algo/experiment_checkpoints')
+EXPERIMENT_CHECKPOINTS_PATH = os.path.join(ROOT_DIR, 'checkpoints/varro/algo')
 
 # Whole population will be written in a pickled
 # dictionary every FREQ generations
