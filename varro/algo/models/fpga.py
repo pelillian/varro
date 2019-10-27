Fixed X scaling bug

varro/algo/models/fpga.py
@@ -33,13 +33,14 @@ class ModelFPGA(Model):
     def predict(self, X, problem=None):
         """Evaluates the model on given data."""
         X = np.asarray(X)
-        if isinstance(X[0], numbers.Real):
+        if isinstance(X[0], numbers.Real) and not isinstance(X[0], numbers.Integral):
             X -= problem.minimum
             X *= 255.0 / (problem.maximum - problem.minimum)
             X = X.astype(int)
 
         y = self.config.evaluate(X)
         #TODO: scale y
+        #if isinstance(X[0], numbers.Real) and not isinstance(X[0], numbers.Integral):
         return y
 
 