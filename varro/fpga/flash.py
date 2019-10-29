8511c1b40f222acdf901493cf6cc765652e833b1
pelillian <plillian@usc.edu>
2019-10-28 22:31:58 -0700

    Raise error if os.system fails

--- varro/fpga/flash.py
+++ varro/fpga/flash.py
@@ -10,9 +10,16 @@ CFG_FILE = "~/sft/share/trellis/misc/openocd/ecp5-evn.cfg"
 def flash_ecp5(file_base_name):
     """Flashes a bitstream file to the ECP5 fpga. For best performance, use a file in ramdisk."""
     # os.system("openocd log_output /dev/null")
-    os.system("openocd -f {0} -c \"transport select jtag; init; svf {1}.svf; exit\" >/dev/null 2>&1".format(CFG_FILE, file_base_name))
+    err = os.system("openocd -f {0} -c \"transport select jtag; init; svf {1}.svf; exit\" >/dev/null 2>&1".format(CFG_FILE, file_base_name))
+    if err:
+        raise RuntimeError("Cannot flash ECP5")
+
+def write_config_file(file_base_name):
+    err = os.system("ecppack --svf {0}.svf {0}.config {0}.bit".format(file_base_name))
+    if err:
+        raise RuntimeError("Cannot create bitstream")
 
 def flash_config_file(file_base_name):
-    os.system("ecppack --svf {0}.svf {0}.config {0}.bit".format(file_base_name))
+    write_config_file(file_base_name)
     flash_ecp5(file_base_name)
 
