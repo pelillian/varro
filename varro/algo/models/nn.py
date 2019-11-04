106a71249329a6615984ee29c1a7bd546f29c703
Chengyi (Jeff) Chen <chen364@usc.edu>
2019-11-03 23:34:27 -0800

    debug: adjusting architecture of nn for regression to make more complex function approximations

--- varro/algo/models/nn.py
+++ varro/algo/models/nn.py
@@ -35,9 +35,9 @@ class ModelNN(Model):
             self.model.add(Dense(problem.output_dim, activation='sigmoid'))
 
         elif problem.approx_type == Problem.REGRESSION:
-            self.model.add(Dense(3, input_dim=problem.input_dim, activation='sigmoid'))
-            self.model.add(Dense(3, activation='sigmoid'))
-            self.model.add(Dense(2, activation='sigmoid'))
+            self.model.add(Dense(12, input_dim=problem.input_dim, activation='sigmoid'))
+            self.model.add(Dense(6, activation='sigmoid'))
+            self.model.add(Dense(4, activation='sigmoid'))
 
             # LAST LAYER:
             # Problem-specific - if y is [-1, 1], use tanh
