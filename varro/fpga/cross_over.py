384aa1d03f156309d03a4bb5866cacd67b9c9387
Chengyi (Jeff) Chen <chen364@usc.edu>
2019-11-24 11:45:04 -0800

    debug: testing crossover

--- varro/fpga/cross_over.py
+++ varro/fpga/cross_over.py
@@ -16,8 +16,8 @@ def cross_over(ind1, ind2):
     ind1_config, ind2_config = FpgaConfig(config_data=ind1.reshape(FPGA_BITSTREAM_SHAPE)), FpgaConfig(config_data=ind2.reshape(FPGA_BITSTREAM_SHAPE))
 
     # Cross-over
-    import pdb; pdb.set_trace()
-    for tile in ind1_config.chip.get_all_tiles():
-        pass
+    point_1_idx, point_2_idx = sorted(np.random.choice(np.arange(0, len(ind1_config.chip.get_all_tiles())), size=2, replace=False))
+    for idx in range(point_1_idx, point_2_idx):
+        ind1_config.chip.swap_tiles(ind1_config.chip.get_all_tiles()[idx].name, ind2_config.chip)
 
     return child1, child2
