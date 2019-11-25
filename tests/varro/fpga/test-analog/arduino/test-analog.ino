6ec2751a1baafb1295183fdf8c118e7a5543e216
seanasyed <seansyed@usc.edu>
2019-11-25 00:38:32 -0800

    inital analog tests

--- tests/varro/fpga/test-analog/arduino/test-analog.ino
+++ tests/varro/fpga/test-analog/arduino/test-analog.ino
@@ -1,7 +1,7 @@
 const int CLOCK_SIGNAL = 13;
 
-int adjacentPorts[] = {A0, A1, A2, A3, A4, A5};
-
+int analogPorts[] = {A0, A1, A2, A3, A4, A5};
+int digitalPorts[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 void setup()
 {
 
@@ -9,8 +9,11 @@ void setup()
   pinMode(CLOCK_SIGNAL, OUTPUT);
   // int output = 0; 
   // Config all pins that will have inputs at some point
-  for (int port : adjacentPorts) {
+  for (int port : analogPorts) {
     pinMode(port, INPUT);
+  }
+  for (int port : digitalPorts) {
+    pinMode(port, OUTPUT);
   }
 }
 
@@ -19,19 +22,41 @@ int portIndex = 0;
 
 void loop()
 {
-  // Alterate the clock signal
-  char buf[50]; 
-  sprintf(buf, "Setting clock signal to %d", output); 
-  Serial.println(buf); 
-  digitalWrite(CLOCK_SIGNAL, output); 
-  output ^= 1; 
-  
-  // print out the pin readings for each analog pin
-  for (int pin : adjacentPorts) {
-    int val = analogRead(pin);
-    sprintf(buf, "%d: %d", pin, val);
-    Serial.println(buf);
-  }
-  Serial.flush();
-  delay(960); 
+    // Read in data from serial buffer
+    char buf[50]; 
+    Serial.readBytes(buf, 50);
+
+    // Activate digital ports based the content of the buffer
+    for (int i = 0; i < sizeof(buf); i++) {
+        
+        // NOTE: We assume that digital pins will only be assigned to 0 or 1
+        char c = buf[i]; 
+        if (c != ',') {
+            if (c == '1') {
+                digitalWrite(digitalPorts[i], HIGH);
+            } else {
+                digitalWrite(digitalPorts[i], LOW);
+            }
+        }
+    }
+
+    // Wait for 100us for generous propagation delay
+    delayMicroseconds(100); 
+
+    // Read values at analog ports
+    int portValues[sizeof(analogPorts)]; 
+    for (int i = 0; i < sizeof(analogPorts); i++) {
+       portValues[i] = analogRead(analogPorts[i]); 
+    } 
+
+    // Assemble serial message assuming there are 6 analog pins
+    sprintf(buf, "%d,%d,%d,%d,%d,%d", portValues[0], portValues[1], portValues[2], portValues[3], portValues[4], portValues[5], portValues[6]);     
+
+    // Send message to serial
+    Serial.println(buf); 
+
+    // Flush serial
+    Serial.flush()
 }
+
+
--- tests/varro/fpga/test-analog/fpga/pin_out.v
+++ tests/varro/fpga/test-analog/fpga/pin_out.v
@@ -1,9 +1,11 @@
-module pin_out (
-p_out, p_in); 
+module test_analog (
+D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, A0, A1, A2, A3, A4, A5)
 
-output p_out;
-input p_in; 
-
-assign p_out = p_in; 
+assign A0 = D0; 
+assign A1 = D1; 
+assign A2 = D2; 
+assign A3 = D3; 
+assign A4 = D4; 
+assign A5 = D5 & D6 & D7 & D8 & D9 & D10 & D11; 
 
 endmodule
