68d8c4e3b2deb067e4a98fed79dc0184b871865a
AlexColello <alexlcolello@gmail.com>
2019-12-08 14:56:07 -0800

    Minor cython optimization

--- varro/cython/fast_cram.pyx
+++ varro/cython/fast_cram.pyx
@@ -1,3 +1,4 @@
+import cython
 cimport numpy as npx
 import numpy as np
 import pytrellis
@@ -5,6 +6,8 @@ import pytrellis
 DTYPE = np.bool
 ctypedef npx.int8_t DTYPE_t
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def load_cram_fast(cram, npx.ndarray[DTYPE_t, ndim=2] config_data):
     cdef int frames = cram.frames()
     cdef int bits_per_frame = cram.bits()
