9e8be270c46f09e0ea130d6fde924f67472aace8
seanasyed <seansyed@usc.edu>
2020-01-07 13:30:02 -0800

    bash script

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ echo $( pwd )
 echo "Compiling cython"
 bash ${DIR}/varro/cython/build_fast_cram.bash
 echo "Flashing Arduino"
-${DIR}/varro/arduino/flash_arduino.sh ${DIR}/varro/arduino/fpga_communication
+
+
+cd ${DIR}/varro/arduino/fpga-communication 
+arduino-cli compile --fqbn arduino:sam:arduino_due_x_dbg
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:sam:arduino_due_x_dbg
