d530674ea25dd74fbaeed9a1a52dbbb5021c92d2
pelillian <plillian@usc.edu>
2020-01-16 16:12:53 -0800

    Test code

--- varro/fpga/test_interface.py
+++
@@ -1,8 +0,0 @@
-from interface import FpgaConfig
-
-testConfig = FpgaConfig()
-print("Attempting to send data [0, 0, 0, 0, 0, 0] to arduino")
-results = testConfig.evaluate([0, 0, 0, 0, 0, 0])
-
-print("Contents of results:")
-print(" ".join(results))
