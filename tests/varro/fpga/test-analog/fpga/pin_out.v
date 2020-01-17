c134414b7a9676ec04cfed4b0947d89d82cbad96
pelillian <plillian@usc.edu>
2020-01-16 20:36:03 -0800

    Fix pin_out test

--- tests/varro/fpga/test-analog/fpga/pin_out.v
+++ tests/varro/fpga/test-analog/fpga/pin_out.v
@@ -5,10 +5,10 @@ input digital0, digital1, digital2, digital3, digital4, digital5, digital6, digi
 output analog0, analog1, analog2, analog3, analog4, analog5;
 
 assign analog0 = digital0; 
-assign analog1 = D1; 
-assign analog2 = D2; 
-assign analog3 = D3; 
-assign analog4 = D4; 
-assign analog5 = D5 & D6 & D7 & D8 & D9 & D10 & D11; 
+assign analog1 = digital1; 
+assign analog2 = digital2; 
+assign analog3 = digital3; 
+assign analog4 = digital4; 
+assign analog5 = digital5 & digital6 & digital7 & digital8 & digital9 & digital10 & digital11; 
 
 endmodule
