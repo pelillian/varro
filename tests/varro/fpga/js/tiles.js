ef3220e4f674e342d2459d323ad5434020861608
Peter Lillian <plillian@usc.edu>
2020-01-23 20:04:03 -0800

    Update tiles.js

--- tests/varro/fpga/js/tiles.js
+++ tests/varro/fpga/js/tiles.js
@@ -1,3 +1,5 @@
+// This script highlights given tiles in the prjtrellis-db docs. https://symbiflow.github.io/prjtrellis-db/
+
 var script = document.createElement("script");
   script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
   script.addEventListener('load', function() {
