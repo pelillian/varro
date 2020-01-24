c8b40ca887f2498ee90664d90c6f05640e1fdaff
Peter Lillian <plillian@usc.edu>
2020-01-23 20:03:16 -0800

    Create tiles.js

---
+++ tests/varro/fpga/js/tiles.js
@@ -0,0 +1,13 @@
+var script = document.createElement("script");
+  script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+
+tiles = ['R2C124', 'R3C124']
+
+tiles.forEach(element => $("em:contains("+element+")").parent().css({"border-color": "#f00", 
+             "border-width":"5px", 
+             "border-style":"dashed"}));
