911be6c0878f5c134ea322b4ae64f6a73f3e92b7
pelillian <plillian@usc.edu>
2020-04-19 14:21:12 -0700

    Comments explaining the scaled and unscaled spaces

--- varro/algo/problems/func_approx.py
+++ varro/algo/problems/func_approx.py
@@ -178,6 +178,10 @@ class ProblemFuncApprox(Problem):
             self.X_train = self.sample_float(xmin, xmax, 0.001)
             self.y_train = self.apply_func(self.X_train)
         elif self.datatype is int:
+            # The scaled space is comprised of integers from 0 to self.values
+            # The unscaled space is comprised of floats from xmin to xmax
+            # Each integer in the scaled space represents a different value in the unscaled space
+            # We have to unscale it to apply the function, and then we have to rescale the output of the function
             self.X_train = self.sample_int(0, self.values) # X_train is scaled
             X_unscaled = self.unscale(self.X_train, values=self.values, unscaled_min=xmin, unscaled_max=xmax)
             y_unscaled = self.apply_func(X_unscaled)
