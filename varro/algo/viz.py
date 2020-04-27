a4b1a08c2a882b4d4978ff73dfda911e4cd63800
patrickjdarrow <patrickjdarrow@gmail.com>
2020-04-26 17:08:07 -0700

    fixed viz to work with xtrue,ytrue,ypred

--- varro/algo/viz.py
+++ varro/algo/viz.py
@@ -1,25 +1,18 @@
 import numpy as np
 import plotly.graph_objects as go
 
-def plot_preds(y_pred,
-               x_true=np.linspace(-2*np.pi, 2*np.pi, 100),
-               y=np.sin):
+def plot_preds(x_true,
+                y_true,
+                y_pred):
   '''
   TODO: animation
 
-  - Args
-      y_pred (np arr):
-          Model outputs of x_true
-      x_true (np arr):
-          True domain
-      y (fn):
-          Maps x to range
   '''
 
   fig = go.Figure()
 
   fig.add_trace(go.Scatter(x=x_true,
-                         y=y(x_true),
+                         y=y_true),
                          name='true',
                          mode='lines'))
   fig.add_trace(go.Scatter(x=x_true,
