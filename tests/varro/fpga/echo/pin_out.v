5c87ccc049d29b58cd8fdf9b186a66682fe44699
seanasyed <seansyed@usc.edu>
2020-05-17 14:10:11 -0700

    wiring digital to analog

--- tests/varro/fpga/echo/pin_out.v
+++ tests/varro/fpga/echo/pin_out.v
@@ -1,13 +1,6 @@
 module sinx (input [11:0] digital, output [5:0] analog, input clk); 
 
-    integer x = 64'd0;  
-    reg [2:0] n = 3'b0; 
-
-    reg state = 1'b0; 
-    
-    always@ (posedge clk) begin
-        analog[5:0] = digital[5:0];
-    end
+    assign analog[5:0] = digital[5:0];    
 
 //    initial begin
 //        while(1) begin
