bb26da200ff72dd320a2a55e023ea8d9325cffc6
Patrick Darrow <patrickjdarrow@gmail.com>
2020-12-14 20:48:05 -0800

    Update grid_search.py

--- varro/algo/hyperparam_opt/grid_search.py
+++ varro/algo/hyperparam_opt/grid_search.py
@@ -17,7 +17,9 @@
 - Parallelize
 '''
 
+import argparse
 from itertools import product
+import numpy as np
 import os
 
 HYPERPARAM_DICT = {}
@@ -32,6 +34,7 @@ def main():
     # IF HYPERPARAMETERS ARE ADDED:
     #    note that aperm indexes hyperparams alphabetically
     for aperm in product(*[*HYPERPARAM_DICT.values()]):
-        os.system('python3 varro/algo/experiment.py --model_type={} --imputpb={} --ngen={} --popsize={} strategy={} --novelty_metric={}'.format(aperm[1], aperm[0], aperm[2], aperm[4], aperm[3], 'hamming' if aperm[4] == 'nsr-es' else Non$
+        print('python3 varro/algo/experiment.py --model_type={} --imputpb={} --ngen={} --popsize={} strategy={} --novelty_metric={}'.format(aperm[1], aperm[0], aperm[2], aperm[4], aperm[3], 'hamming' if aperm[4] == 'nsr-es' else None))
+
 if __name__ == '__main__':
     main()
