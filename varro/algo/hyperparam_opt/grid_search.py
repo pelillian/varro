8ea49fd8f6b370a751339aaa36bc538e8d3831f1
Patrick Darrow <patrickjdarrow@gmail.com>
2020-12-22 19:15:25 -0800

    added problem type

--- varro/algo/hyperparam_opt/grid_search.py
+++ varro/algo/hyperparam_opt/grid_search.py
@@ -28,6 +28,7 @@ HYPERPARAM_DICT['imutpb'] = [1e-4, 5e-5, 1e-5]
 HYPERPARAM_DICT['ngen'] = [50]
 HYPERPARAM_DICT['popsize'] = [100, 30, 5]
 HYPERPARAM_DICT['strategy'] = ['sga', 'nsr-es']
+HYPERPARAM_DICT['problem_type'] = ['simple_step']
 
 def main():
     # fit for each argument permutation
@@ -35,7 +36,7 @@ def main():
     #    note that aperm indexes hyperparams alphabetically
     for idx, aperm in enumerate(product(*[*HYPERPARAM_DICT.values()])):
         print('Experiment ', idx)
-        os.system('python3 -m varro.algo.experiment --model_type=\'{}\' --imutpb={} --ngen={} --popsize={} --strategy=\'{}\' --novelty_metric=\'{}\''.format(aperm[0], aperm[1], aperm[2], aperm[3], aperm[4], 'hamming' if aperm[4] == 'nsr-es' else None))
+        os.system('python3 -m varro.algo.experiment --model_type=\'{}\' --imutpb={} --ngen={} --popsize={} --strategy=\'{}\' --problem_type=\'{}\' --novelty_metric=\'{}\''.format(aperm[0], aperm[1], aperm[2], aperm[3], aperm[4], aperm[5], 'hamming' if aperm[4] == 'nsr-es' else None))
 
 if __name__ == '__main__':
     main()
