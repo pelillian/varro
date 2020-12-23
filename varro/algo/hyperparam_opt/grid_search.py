2dffb168157312cbaef9807baac18d50ea12764c
Patrick Darrow <patrickjdarrow@gmail.com>
2020-12-22 19:32:01 -0800

    updated HPs

--- varro/algo/hyperparam_opt/grid_search.py
+++ varro/algo/hyperparam_opt/grid_search.py
@@ -24,7 +24,7 @@ import os
 
 HYPERPARAM_DICT = {}
 HYPERPARAM_DICT['model_type'] = ['fpga']
-HYPERPARAM_DICT['imutpb'] = [1e-4, 5e-5, 1e-5]
+HYPERPARAM_DICT['imutpb'] = [1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
 HYPERPARAM_DICT['ngen'] = [50]
 HYPERPARAM_DICT['popsize'] = [100, 30, 5]
 HYPERPARAM_DICT['strategy'] = ['sga', 'nsr-es']
@@ -36,7 +36,7 @@ def main():
     #    note that aperm indexes hyperparams alphabetically
     for idx, aperm in enumerate(product(*[*HYPERPARAM_DICT.values()])):
         print('Experiment ', idx)
-        os.system('python3 -m varro.algo.experiment --model_type=\'{}\' --imutpb={} --ngen={} --popsize={} --strategy=\'{}\' --problem_type=\'{}\' --novelty_metric=\'{}\''.format(aperm[0], aperm[1], aperm[2], aperm[3], aperm[4], aperm[5], 'hamming' if aperm[4] == 'nsr-es' else None))
+        os.system('python -m varro.algo.experiment --purpose=fit --mutpb 1.0 --imutpb_decay=0.95 --model_type=\'{}\' --imutpb={} --ngen={} --popsize={} --strategy=\'{}\' --problem_type=\'{}\' --novelty_metric=\'{}\''.format(aperm[0], aperm[1], aperm[2], aperm[3], aperm[4], aperm[5], 'hamming' if aperm[4] == 'nsr-es' else None))
 
 if __name__ == '__main__':
     main()
