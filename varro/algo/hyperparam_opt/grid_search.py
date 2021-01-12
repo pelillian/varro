ac16702d781f665bbdcf5555f9b3b9ab61df136d
pelillian <plillian@usc.edu>
2021-01-11 20:58:42 -0800

    Hyperparameters

--- varro/algo/hyperparam_opt/grid_search.py
+++ varro/algo/hyperparam_opt/grid_search.py
@@ -25,8 +25,8 @@ import os
 HYPERPARAM_DICT = {}
 HYPERPARAM_DICT['model_type'] = ['fpga']
 HYPERPARAM_DICT['imutpb'] = [1e-3, 1e-4, 1e-5]
-HYPERPARAM_DICT['ngen'] = [50]
-HYPERPARAM_DICT['popsize'] = [100, 30, 5]
+HYPERPARAM_DICT['ngen'] = [25]
+HYPERPARAM_DICT['popsize'] = [30, 5]
 HYPERPARAM_DICT['strategy'] = ['sga', 'nsr-es']
 HYPERPARAM_DICT['problem_type'] = ['simple_step']
 HYPERPARAM_DICT['imutpb_decay'] = [0.8, 0.9, 0.95, 0.99]
@@ -38,7 +38,7 @@ def main():
     #    note that aperm indexes hyperparams alphabetically
     for idx, aperm in enumerate(product(*[*HYPERPARAM_DICT.values()])):
         print('Experiment ', idx)
-        os.system('python -m varro.algo.experiment --ckpt_freq=1000 --purpose=fit --mutpb 1.0 --model_type=\'{}\' --imutpb={} --ngen={} --popsize={} --strategy=\'{}\' --problem_type=\'{}\' --imutpb_decay={} --novelty_metric=\'{}\' --lambda_penalty=\'{}\''.format(aperm[0], aperm[1], aperm[2], aperm[3], aperm[4], aperm[5], aperm[6], 'hamming' if aperm[4] == 'nsr-es' else None, aperm[7]))
+        os.system('python -m varro.algo.experiment --ckpt_freq=1000 --purpose=fit --mutpb 1.0 --model_type=\'{}\' --imutpb={} --ngen={} --popsize={} --strategy=\'{}\' --problem_type=\'{}\' --imutpb_decay={} --novelty_metric=\'{}\' --lambda_penalty=\'{}\' --halloffamesize=0.2'.format(aperm[0], aperm[1], aperm[2], aperm[3], aperm[4], aperm[5], aperm[6], 'hamming' if aperm[4] == 'nsr-es' else None, aperm[7]))
 
 if __name__ == '__main__':
     main()
